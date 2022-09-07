12ebf8650c25d798e91071848d518a295feae4f2
chaya2z <chaya2zdev@gmail.com>
2022-09-07 17:33:14 +0900

    [ REMOVE ] unused variable

--- tfstate_bucket/variables.tf
+++ tfstate_bucket/variables.tf
@@ -1,13 +1,3 @@
-#
-#  See https://www.terraform.io/intro/getting-started/variables.html for more details.
-#
-
-#  Change these defaults to fit your needs!
-
-variable "esxi_hostname" {
-  default = "192.168.0.4"
-}
-
 variable "minio_access_key" {}
 
 variable "minio_secret_key" {}
